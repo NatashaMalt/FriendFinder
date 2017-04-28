1696c10355fbb0f372db259a2e4d2c6e9970c592
Natasha Malt <natashamalt@calvisitor-10-105-160-243.calvisitor.1918.berkeley.edu>
2017-04-27 20:38:18 -0700

    survey done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,52 @@
 var kittyPals = [
 {"name": "Ms. Marple",
  "photo": "https://pixabay.com/en/cat-russian-blue-look-eyes-gray-1435590/",
-
-
+ "scores": [
+  1,
+  5,
+  5,
+  3,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  ]
 }
 
 {"name": "H. Poirot",
  "photo": "https://pixabay.com/en/cat-tuxedo-portrait-pet-domestic-2066415/",
+ "scores": [
+  5,
+  3,
+  2,
+  1,
+  1,
+  1,
+  1,
+  1,
+  5,
+  1
+  ]
+
 
 }
 
 {"name": "Hastings",
  "photo": "https://pixabay.com/en/brown-gray-cat-tabby-kitten-pets-834287/",
+ "scores" : [
+ 1,
+ 2,
+ 3,
+ 4,
+ 5,
+ 5,
+ 4,
+ 3,
+ 2,
+ 1,
+ ]
 
 }
 
